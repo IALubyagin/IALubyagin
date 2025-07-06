5157f97db080e12e45504592372b1c366d5f7120
igorlubyagin <hammavet@hotmail.com>
2025-07-06 21:06:47 +0300

    First Local Changes

--- work.py
+++ work.py
@@ -1,1 +1,7 @@
+# git clone git@github.com:IALubyagin/IALubyagin.git
+# git clone https://github.com/IALubyagin/IALubyagin.git
+# git remote set-url origin git@github.com:IALubyagin/IALubyagin.git
+# git remote set-url origin https://github.com/IALubyagin/IALubyagin.git
+# git add work.py
+# git commit -m "First Local Changes"
 print("Hello!")
