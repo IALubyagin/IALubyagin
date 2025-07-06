6ea5a1ab1d1da4fb13f316c92b16d3bb5cabeb31
igorlubyagin <hammavet@hotmail.com>
2025-07-06 21:25:01 +0300

    Third Local Changes

--- work.py
+++ work.py
@@ -4,6 +4,7 @@
 # git remote set-url origin https://github.com/IALubyagin/IALubyagin.git
 # git add work.py
 # git commit -m "First Local Changes"
-
+# git push origin main
+# git fetch + git merge = git pull
 
 print("Hello, AIOgram TG Bot!")
